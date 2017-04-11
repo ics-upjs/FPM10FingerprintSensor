981ae8736bde88bf6b17b1c9da26f1c339926154
František Galčík <frantisek.galcik@gbox.sk>
2017-04-11 23:15:36 +0200

    Add sample project.

---
+++ examples/enrollment/enrollment/src/main/java/sk/upjs/zirro/fingerprint/enrollment/App.java
@@ -0,0 +1,38 @@
+package sk.upjs.zirro.fingerprint.enrollment;
+
+import sk.upjs.zirro.fpm10sensor.FingerprintSensor;
+import sk.upjs.zirro.fpm10sensor.HumanActionListener;
+
+public class App {
+	public static void main(String[] args) {
+		FingerprintSensor sensor = new FingerprintSensor("/dev/ttyUSB0");
+		sensor.open();
+		HumanActionListener hal = new HumanActionListener() {
+			@Override
+			public void putFinger() {
+				System.out.println("Place finger on the sensor.");
+			}
+
+			@Override
+			public void removeFinger() {
+				System.out.println("Remove finger.");
+			}
+		};
+
+		try {
+			sensor.downloadImageWorkflow("prst.png", hal);
+			// sensor.enrollWorkflow(11,hal);
+			FingerprintSensor.SearchResult result = sensor.uploadImageAndSearchWorkflow("prst.png", hal);
+			if (result == null) {
+				System.out.println("No match!");
+			} else {
+				System.out.println("Found id : " + result.getId() + ", matchScore : " + result.getMatchScore());
+			}
+
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			sensor.close();
+		}
+	}
+}
